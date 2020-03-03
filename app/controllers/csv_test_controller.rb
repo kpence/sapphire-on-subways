0d806cecdb82fa6b5dc572b211d5c74bcf4b272c
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-02 20:04:49 -0600

    filtering the csv

--- app/controllers/csv_test_controller.rb
+++ app/controllers/csv_test_controller.rb
@@ -4,7 +4,7 @@ class CsvTestController < ApplicationController
   def index
     items = []
     CSV.foreach('app/assets/test.csv', headers: true) do |row|
-      items << row.to_h
+			items << row.to_h.select {|entry| entry != "TOTAL" && entry != nil}
     end
     @items = items
   end
