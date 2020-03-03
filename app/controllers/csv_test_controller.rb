a8262f82c672dd1e4c831ee1d54a07e60c116dda
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-02 19:21:06 -0600

    creating new testing branch

---
+++ app/controllers/csv_test_controller.rb
@@ -0,0 +1,11 @@
+# require 'CSV'
+
+class CsvTestController < ApplicationController
+  def index
+    items = []
+    CSV.foreach('app/assets/test.csv', headers: true) do |row|
+      items << row.to_h
+    end
+    @items = items
+  end
+end
