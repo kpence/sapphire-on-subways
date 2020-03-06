9ec9051699298671b39e453470b412a3d916934d
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-06 15:38:27 -0600

    Adding migrations

---
+++ db/migrate/20200306210930_create_dances.rb
@@ -0,0 +1,25 @@
+class CreateDances < ActiveRecord::Migration[5.0]
+  def up
+    create_table :dances do |t|
+      t.references :performance
+      t.references :dancer
+    end
+
+    create_table :dancers do |t|
+      t.string :name
+    end
+
+    create_table :performances do |t|
+      t.string :name
+      t.references :act
+    end
+
+    create_table :acts do |t|
+      t.integer :number
+    end
+
+    create_table :schedules do |t|
+      t.string :filename
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200306210930) do
+
+  create_table "acts", force: :cascade do |t|
+    t.integer "number"
+  end
+
+  create_table "dancers", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "dances", force: :cascade do |t|
+  end
+
+  create_table "performances", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string "filename"
+  end
 
 end
