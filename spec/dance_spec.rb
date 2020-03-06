f1b7d180e137dd73ef6fe7d59b37fd334da0d2a4
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-06 16:21:44 -0600

    Changed the rspec

--- spec/dance_spec.rb
+++ spec/dance_spec.rb
@@ -2,10 +2,10 @@ require_relative './rails_helper.rb'
 require_relative '../app/models/dance.rb'
 
 describe Dance, type: :model do 
-    it 'has a schedule it belongs to' do
-        should belong_to(:schedule)
+    it 'has a performance it belongs to' do
+        should belong_to(:performance)
     end
-    it 'has dancers and belongs to dancers' do
-        should have_and_belong_to_many(:dancers)
+    it 'has a dancer it belongs to' do
+        should belong_to(:dancer)
     end
 end
