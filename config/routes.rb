c42c8fe6f66784d4b066682e33a302944ca9ee1d
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-12 03:22:09 -0500

    Removing superfluous code in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Basic route for root page
   
-  get 'schedules/import' => 'schedules#upload_a_csv'
   resources :schedules do
     collection {post :import}
   end
