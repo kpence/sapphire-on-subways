9a05eb66e3b7e8b97102fc6a09482d6c3ae73fe2
Troy Fulton <troyfulton@tamu.edu>
2020-03-23 05:35:42 +0000

    Wrote tests to cover performance sorting

--- spec/controllers/performances_controller_spec.rb
+++ spec/controllers/performances_controller_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 describe PerformancesController do
-    #describe "#method" do
-    #   it 'should do something'
-    #end
+  describe "#sort" do
+    fixtures :performances
+    before :each do
+      @fake_perf1 = performances(:MyPerf1)
+      @fake_perf2 = performances(:MyPerf2)
+      @fake_perf3 = performances(:MyPerf3)
+      @fake_performances = [@fake_perf2, @fake_perf3, @fake_perf1]
+    end
+    
+    it 'should update all the performances positions by id' do
+      @fake_performances.each_with_index do |perf, index|
+        expect(Performance).to receive(:where).with({id: perf.id.to_s}).and_return(perf)
+        expect(perf).to receive(:update).with(position: index + 1)
+      end
+      
+      put :sort, {params: {:performance => @fake_performances}}
+    end
+  end
 end
