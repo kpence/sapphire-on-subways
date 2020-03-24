d7837edf24b69fdf31ef15a7f01db90bb81d0ff4
Troy Fulton <troyfulton@tamu.edu>
2020-03-24 19:24:13 +0000

    New working (albeit slowly) algorithm for scheduling

--- app/controllers/concerns/scheduler.rb
+++ app/controllers/concerns/scheduler.rb
@@ -41,7 +41,7 @@ module Scheduler
     end
   end
   
-  def score(permutation)
+  def score_perm(permutation)
     # First form the schedule
     temp_schedule = []
     permutation.each do |index|
@@ -53,8 +53,8 @@ module Scheduler
     second_index = 1
     curr_score = 0
     while second_index < temp_schedule.length()
-      first_id = @performances[first_index].id
-      second_id = @performances[second_index].id
+      first_id = temp_schedule[first_index].id
+      second_id = temp_schedule[second_index].id
       min_id = [first_id, second_id].min
       max_id = [first_id, second_id].max
       curr_score += @graph[min_id][max_id]
@@ -64,22 +64,45 @@ module Scheduler
     return curr_score
   end
   
+  def permute(original_order, num_perms)
+    perms = [original_order]
+    (1..num_perms).to_a.each do
+      # Find a new ordering
+      new_order = original_order.shuffle
+      while perms.include? new_order
+        new_order = original_order.shuffle
+      end
+      perms.append(new_order)
+    end
+    return perms
+  end
+  
   def minimize_conflicts(schedule)
     @schedule = schedule
     @performances = @schedule.acts[0].performances
     form_graph
+    puts "Graph: " + @graph.to_s
+    original_order = (0..@performances.length() - 1).to_a
+    puts "Original order: " + original_order.to_s
+    
     puts "Generating permutations..."
-    perms = 
-    min_score = score(perms[0])
+    num_perms = 9999
+    perms = permute(original_order, num_perms)
+    puts "Number of Permutations: " + perms.length().to_s + "..."
+    
+    min_score = score_perm(perms[0])
     min_idx = 0
     perms.each_with_index do |perm, i|
       score = score_perm(perm)
-      if score < min_score || score < 25
+      if score < min_score
         min_score = score
         min_idx = i
+      #else
+        #puts "Score " + score.to_s + " for order " + perm.to_s
       end
     end
     winner_permutation = perms[min_idx]
+    puts "Winner order: " + winner_permutation.to_s + " with score " + min_score.to_s
     curr_pos = 1
     winner_permutation.each do |winner_index|
       @performances[winner_index].position = curr_pos
