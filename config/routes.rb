2556d941b099ed5650d006188d7834f89e64e33b
hudsonhumphries <hthumphries@gmail.com>
2020-03-26 04:17:41 +0000

    Updating routing for performances_path, don't have a work around in place anymore.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   resources :schedules do
     collection {
       post :import
-      post :insert, :to => 'schedules#insert'
     }
   end
   
   resources :performances do
     collection {
       put :sort
-      post :insert, :to => 'performances#create'
     }
   end
 
