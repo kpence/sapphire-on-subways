7d86442b5d8300a5899dd190f204b384235f0d46
ki pi <kpence@Kyles-MacBook-Pro.local>
2020-03-28 13:07:14 -0500

    fixed bug in drag and drop

--- app/assets/javascripts/drag_and_drop_schedule.js
+++ app/assets/javascripts/drag_and_drop_schedule.js
@@ -2,7 +2,7 @@
 document.addEventListener("turbolinks:load", function () {
   
   $(".sortable").sortable({
-    items: "tr:not(.unmoveable)",
+    items: "tr:not(.unmoveable .droppable)",
     update: function(e, ui) {
       if (this === ui.item.parent()[0]) { // This prevents the method from being called twice when moving between acts
         Rails.ajax({
@@ -19,7 +19,7 @@ document.addEventListener("turbolinks:load", function () {
     accept: ".sortable tr",
     hoverClass: "droppable-hover",
     drop: function(e, ui) {
-      console.log($(this).data("url")+"&move_perf="+e.toElement.id.substr(12))
+      //console.log($(this).data("url")+"&move_perf="+e.toElement.id.substr(12), "e", e, "ui", ui, "this")
       Rails.ajax({
         url: $(this).data("url")+"&move_perf="+e.toElement.id.substr(12),
         type: "PUT",
