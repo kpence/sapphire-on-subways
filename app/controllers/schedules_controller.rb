d91bb402ad939370b0a4ca9e1c56dbd3a415d34c
Jeevika Jarmarwala <jeevika04@gmail.com>
2020-04-11 21:18:52 +0000

    Moved insert to bottom

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -85,6 +85,9 @@ class SchedulesController < ApplicationController
       end
       @conflicts[act.number] = self.conflicts(act.number)
     end
+    @act_classes = {}
+    @act_classes[1] = "floatLeft"
+    @act_classes[2] = "floatRight"
 
 
 
